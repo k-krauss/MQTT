e097f09227f3fe1380062578efaa78b0bf1a952a
k-krauss <k.krauss@vrades-maschinenbau.com>
2025-04-14 15:20:45 +0200

    Publish/Subscribe implementiert.

--- MQTT/Form1.Designer.cs
+++ MQTT/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MQTT
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.txtLog = new RichTextBox();
+            this.btnConnect = new Button();
+            this.btnDisconnect = new Button();
+            this.btnClearLog = new Button();
+            this.btnSubscribe = new Button();
+            this.btnUnsubscribe = new Button();
+            this.btnPublish = new Button();
+            SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = DockStyle.Top;
+            this.txtLog.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.txtLog.Location = new Point(0, 0);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new Size(1195, 408);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            this.btnConnect.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.btnConnect.Location = new Point(12, 507);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new Size(139, 44);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Verbinden";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += btnConnect_Click;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            this.btnDisconnect.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.btnDisconnect.Location = new Point(1044, 507);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new Size(139, 44);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Verbindung trennen";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += btnDisconnect_Click;
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.btnClearLog.Location = new Point(12, 414);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new Size(139, 44);
+            this.btnClearLog.TabIndex = 3;
+            this.btnClearLog.Text = "Log leeren";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += btnClearLog_Click;
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            this.btnSubscribe.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.btnSubscribe.Location = new Point(157, 507);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new Size(139, 44);
+            this.btnSubscribe.TabIndex = 4;
+            this.btnSubscribe.Text = "Subscribe";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += btnSubscribe_Click;
+            // 
+            // btnUnsubscribe
+            // 
+            this.btnUnsubscribe.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            this.btnUnsubscribe.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.btnUnsubscribe.Location = new Point(302, 507);
+            this.btnUnsubscribe.Name = "btnUnsubscribe";
+            this.btnUnsubscribe.Size = new Size(139, 44);
+            this.btnUnsubscribe.TabIndex = 5;
+            this.btnUnsubscribe.Text = "Unsubscribe";
+            this.btnUnsubscribe.UseVisualStyleBackColor = true;
+            this.btnUnsubscribe.Click += btnUnsubscribe_Click;
+            // 
+            // btnPublish
+            // 
+            this.btnPublish.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            this.btnPublish.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            this.btnPublish.Location = new Point(447, 507);
+            this.btnPublish.Name = "btnPublish";
+            this.btnPublish.Size = new Size(139, 44);
+            this.btnPublish.TabIndex = 6;
+            this.btnPublish.Text = "Publish";
+            this.btnPublish.UseVisualStyleBackColor = true;
+            this.btnPublish.Click += btnPublish_Click;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(1195, 563);
+            this.Controls.Add(this.btnPublish);
+            this.Controls.Add(this.btnUnsubscribe);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.btnClearLog);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtLog);
+            this.Name = "FrmMain";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "MQTT";
+            this.WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private RichTextBox txtLog;
+        private Button btnConnect;
+        private Button btnDisconnect;
+        private Button btnClearLog;
+        private Button btnSubscribe;
+        private Button btnUnsubscribe;
+        private Button btnPublish;
     }
 }
--- MQTT/Form1.cs
+++ MQTT/Form1.cs
@@ -1,10 +1,243 @@
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using System.Text;
+
 namespace MQTT
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        private const string url = "test.mosquitto.org";
+        private const int port = 1883;
+
+        private IMqttClient mqttClient;
+        private IMqttClientOptions mqttOptions;
+
+        public FrmMain()
         {
             InitializeComponent();
         }
+
+        #region Methoden
+
+        private void InitializeMqttClient()
+        {
+            MqttFactory mqttFactory = new MqttFactory();
+            mqttClient = mqttFactory.CreateMqttClient();
+            mqttOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(url, port)
+                //.WithCredentials("rw", "readwrite")
+                .WithCleanSession()
+                .Build();
+
+            mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(async e =>
+            {
+                if (txtLog.InvokeRequired)
+                {
+                    txtLog.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText("Verbunden mit dem Broker.\r\n");
+                    });
+                }
+                else
+                {
+                    txtLog.AppendText("Verbunden mit dem Broker.\r\n");
+                }
+            });
+
+            mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(e =>
+            {
+                if (txtLog.InvokeRequired)
+                {
+                    txtLog.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText("Verbindung zum Broker getrennt.\r\n");
+                    });
+                }
+                else
+                {
+                    txtLog.AppendText("Verbindung zum Broker getrennt.\r\n");
+                }
+                return Task.CompletedTask;
+            });
+        }
+
+        private async void SubscribeToTopic(string topic)
+        {
+            if (mqttClient != null && mqttClient.IsConnected)
+            {
+                await mqttClient.SubscribeAsync(new MQTTnet.Client.Subscribing.MqttClientSubscribeOptionsBuilder()
+                    .WithTopicFilter(topic)
+                    .Build());
+
+                InitializeMessageHandler();
+
+                txtLog.AppendText($"Thema '{topic}' abonniert.\r\n");
+            }
+            else
+            {
+                txtLog.AppendText("Client ist nicht verbunden. Abonnement fehlgeschlagen.\r\n");
+            }
+        }
+
+        private void InitializeMessageHandler()
+        {
+            mqttClient.ApplicationMessageReceivedHandler = new MQTTnet.Client.Receiving.MqttApplicationMessageReceivedHandlerDelegate(e =>
+            {
+                if (e.ApplicationMessage.Topic != null && e.ApplicationMessage.Payload != null)
+                {
+                    string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    string topic = e.ApplicationMessage.Topic;
+
+                    if (txtLog.InvokeRequired)
+                    {
+                        txtLog.Invoke((MethodInvoker)delegate
+                        {
+                            txtLog.AppendText($"Nachricht empfangen:\r\nThema: {topic}\r\nNachricht: {payload}\r\n");
+                        });
+                    }
+                    else
+                    {
+                        txtLog.AppendText($"Nachricht empfangen:\r\nThema: {topic}\r\nNachricht: {payload}\r\n");
+                    }
+                }
+            });
+        }
+
+        private async void UnsubscribeFromTopic(string topic)
+        {
+            if (mqttClient != null && mqttClient.IsConnected)
+            {
+                await mqttClient.UnsubscribeAsync(topic);
+                if (txtLog.InvokeRequired)
+                {
+                    txtLog.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText($"Thema '{topic}' abbestellt.\r\n");
+                    });
+                }
+                else
+                {
+                    txtLog.AppendText($"Thema '{topic}' abbestellt.\r\n");
+                }
+            }
+            else
+            {
+                if (txtLog.InvokeRequired)
+                {
+                    txtLog.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText("Client ist nicht verbunden. Abbestellung fehlgeschlagen.\r\n");
+                    });
+                }
+                else
+                {
+                    txtLog.AppendText("Client ist nicht verbunden. Abbestellung fehlgeschlagen.\r\n");
+                }
+            }
+        }
+
+        private async void PublishMessage(string topic, string message)
+        {
+            if (mqttClient != null && mqttClient.IsConnected)
+            {
+                var mqttMessage = new MQTTnet.MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(message)
+                    .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                    .Build();
+
+                await mqttClient.PublishAsync(mqttMessage);
+                if (txtLog.InvokeRequired)
+                {
+                    txtLog.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText($"Nachricht an '{topic}' gesendet: {message}\r\n");
+                    });
+                }
+                else
+                {
+                    txtLog.AppendText($"Nachricht an '{topic}' gesendet: {message}\r\n");
+                }
+            }
+            else
+            {
+                if (txtLog.InvokeRequired)
+                {
+                    txtLog.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText("Client ist nicht verbunden. Nachricht konnte nicht gesendet werden.\r\n");
+                    });
+                }
+                else
+                {
+                    txtLog.AppendText("Client ist nicht verbunden. Nachricht konnte nicht gesendet werden.\r\n");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (mqttClient == null)
+            {
+                InitializeMqttClient();
+            }
+
+            if (!mqttClient.IsConnected)
+            {
+                try
+                {
+                    await mqttClient.ConnectAsync(mqttOptions);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                txtLog.AppendText("Client ist bereits verbunden.\r\n");
+            }
+        }
+
+        private async void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (mqttClient.IsConnected)
+            {
+                await mqttClient.DisconnectAsync();
+            }
+            else
+            {
+                txtLog.AppendText("Client ist bereits getrennt.\r\n");
+            }
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = String.Empty;
+        }
+
+        private void btnSubscribe_Click(object sender, EventArgs e)
+        {
+            SubscribeToTopic("/mein/mqtt/test");
+        }
+
+        private void btnUnsubscribe_Click(object sender, EventArgs e)
+        {
+            UnsubscribeFromTopic("/mein/mqtt/test");
+        }
+
+        private void btnPublish_Click(object sender, EventArgs e)
+        {
+            PublishMessage("/mein/mqtt/test", "Das ist ein Test");
+        }
+
+        #endregion
     }
 }
